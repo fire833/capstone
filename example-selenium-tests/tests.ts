1327f6b8b3effd3adfb9c655cf911a3228da738d
Aaron <aaronmp01@gmail.com>
2023-02-05 21:30:55 -0600

    add example selenium tests, for testing the testing facilities

---
+++ example-selenium-tests/tests.ts
@@ -0,0 +1,22 @@
+import { Builder, Capabilities } from "selenium-webdriver"
+import { writeFileSync } from "fs";
+
+async function runTest(){
+    let driver = await new Builder().forBrowser('chrome').build(); 
+    let num = Math.floor(Math.random() * 100)
+    await driver.get("https://google.com/search?q=newtest" + num);
+    let title = await driver.getTitle();
+    console.log("Ran test I guess " + title);
+
+    
+    let file = await driver.takeScreenshot();
+    writeFileSync(`./ss/ss-${num}.png`, Buffer.from(file, 'base64'));
+    await driver.quit();
+}
+
+
+console.log("Don't forget to set SELENIUM_REMOTE_URL, it is currently set to: " + process.env['SELENIUM_REMOTE_URL']);
+
+for(let i = 0; i < 50; i++){
+    runTest();
+}
