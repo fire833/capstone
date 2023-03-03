5ea8450196283f4620687fd490613c1254d26bf2
joshstenis <joshstenis@gmail.com>
2023-03-02 23:38:00 -0600

    Compartmentalize terraform files into folder

---
+++ terraform/gke.tf
@@ -0,0 +1,41 @@
+variable "gke_username" {
+  default = ""
+  description = "gke_username"
+}
+
+variable "gke_password" {
+  default = ""
+  descript = "gke_password"
+}
+
+resource "google_container_cluster" "primary" {
+  # Define in different TF file ?
+  name = ""
+  location = ""
+
+  # Edit later
+  initial_node_count = 1
+
+  # Define in different TF file
+  network = 
+  subnetwork = 
+}
+
+# May need to use one of the helm charts for this
+resource "google_container_node_pool" "primary_nodes" {
+
+  name = 
+  location = 
+  cluster = 
+  node_count = 
+
+  node_config {
+    oauth_scopes = []
+
+    labels = {}
+
+    machine_type = 
+    tags = 
+  }
+  
+}
