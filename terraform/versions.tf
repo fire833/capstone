8cf2073a455f3a0e17a8ed91e4059e2afe12e209
joshstenis <joshstenis@gmail.com>
2023-03-14 00:08:41 -0500

    Added helm required_provider block in versions.tf .

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,6 +4,11 @@ terraform {
         source = "hashicorp/google"
         version = "4.27.0"
       }
+      
+      helm = {
+        source = "hashicorp/helm"
+        version = ">= 3.3.2"
+      }
     }
 
     required_version = ">= 0.14"
