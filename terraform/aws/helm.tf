f78957cbc13cadb873cb14ee2fba6871f09414bf
fire833 <kttpsy@gmail.com>
2023-03-17 21:57:54 -0500

    [chart] Fixed chart collision of servicePorts.
    Signed-off by: Kendall Tauser <kttpsy@gmail.com>

--- terraform/aws/helm.tf
+++ terraform/aws/helm.tf
@@ -4,6 +4,8 @@ resource "helm_release" "grid_cluster" {
   repository = "https://fire833.github.io/capstone/"
   chart      = "selenium-grid-cluster"
 
+  version = "0.3.5"
+
   set {
     name  = "cloud_provider"
     value = "AWS"
