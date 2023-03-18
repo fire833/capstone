dfbc6de112225876e9937b643e4bea95322651ae
fire833 <kttpsy@gmail.com>
2023-03-17 21:53:02 -0500

    [aws] Got cluster up and running in AWS.
    Signed-off by: Kendall Tauser <kttpsy@gmail.com>

--- terraform/aws/helm.tf
+++ terraform/aws/helm.tf
@@ -9,21 +9,3 @@ resource "helm_release" "grid_cluster" {
     value = "AWS"
   }
 }
-
-resource "helm_release" "nginx_ingress" {
-  name       = "aws-load-balancer"
-  repository = "https://aws.github.io/eks-charts/"
-  chart      = "aws-load-balancer-controller"
-
-  namespace = "kube-system"
-
-  set {
-    name = "clusterName"
-    value = module.eks.cluster_name
-  }
-
-  # set {
-  #   name = "serviceAccount.create"
-  #   value = true
-  # }
-}
