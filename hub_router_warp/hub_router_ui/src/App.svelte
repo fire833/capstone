61e2a1d4ebdaab911b9266e58d8fb7263c8b1446
Aaron <aaronmp01@gmail.com>
2023-04-09 19:43:55 -0500

    [HubRouter UI] Move from SvelteKit to base Svelte

---
+++ hub_router_warp/hub_router_ui/src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { api_data } from "./lib/data";
+</script>
+
+
+<h1>Welcome to SvelteKit 3!</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>API responded with {JSON.stringify($api_data)}</p>
