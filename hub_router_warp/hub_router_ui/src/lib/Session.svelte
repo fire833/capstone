7425540572a65aeaac281889dca9fedd581a7431
Aaron <aaronmp01@gmail.com>
2023-04-12 12:06:25 -0500

    [HubRouter UI] WIP basic UI

---
+++ hub_router_warp/hub_router_ui/src/lib/Session.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+    import type { SlotsEntity } from "./data";
+    export let slot: SlotsEntity;
+    import Chrome from "../assets/chrome.191aefd5192c43508fa5f86da6808929.svelte";
+    import Edge from "../assets/edge.d2a278165ff8e7dcc4af17246954a0e1.svelte";
+    import Firefox from "../assets/firefox.efda58979e042bab7c689eab277b5a5d.svelte";
+    import Opera from "../assets/opera.b65596581939839e3bb1bd80706b9e45.svelte";
+    import Safari from "../assets/safari.b8bf72ee61f58c60edb2ffa27b172d07.svelte";
+
+    let browser_name_to_icon = {
+        chrome: Chrome,
+        googlechrome: Chrome,
+        firefox: Firefox,
+        mozillafirefox: Firefox,
+        microsoftedge: Edge,
+        safari: Safari,
+    };
+</script>
+
+<div class="slot-wrapper">
+    <div class="slot-row">
+        <div class="svg-override">
+            <svelte:component
+                this={browser_name_to_icon[
+                    slot.stereotype.browserName.toLowerCase()
+                ]}
+                width={69}
+            />
+        </div>
+        {#if slot.session === null}
+            <h3 class="slot-h3">Slot</h3>
+        {:else}
+            <p>Session: {JSON.stringify(slot.session)}</p>
+        {/if}
+    </div>
+</div>
+
+<style>
+    .slot-wrapper {
+        width: 100%;
+    }
+
+    :global(.svg-override > svg) {
+        width: 1.5em;
+        height: 1.5em;
+        vertical-align: middle;
+    }
+
+    .svg-override {
+        display: inline-block;
+        vertical-align: middle;
+    }
+
+    .slot-h3 {
+        display: inline-block;
+        padding-left: 0.5em;
+    }
+</style>
--- hub_router_warp/hub_router_ui/src/lib/data.ts
+++ hub_router_warp/hub_router_ui/src/lib/data.ts
@@ -1,30 +1,171 @@
 import { writable, type Writable } from "svelte/store"
 
-export type ApiSchema = {[idx: string]: any};
+export type ApiSchema = { [idx: string]: any };
 
 export type ApiFetchError = {
-    is_error: true,
+    state: "Error",
     message: string
 }
 
-export const api_data = writable<undefined | ApiSchema | ApiFetchError>(undefined);
+export type ApiResponse = {
+    state: "Success",
+    data: ApiStatusData[]
+}
 
-fetch(`${window.location.origin}/api/hubs`)
-    .then(e => {
-        if(!e.ok){
-            throw new Error(`Got invalid response fetching ${e.url}:  ${e.status} ${e.statusText}`)
+export type ApiLoading = {
+    state: "Loading"
+}
+
+export const api_data = writable<ApiLoading | ApiResponse | ApiFetchError>({ state: "Loading" });
+export const  breadcrumb = writable<number[]>([0, 1, 0]);
+
+export interface HubInfo {
+    value: HubInfoValue;
+}
+
+export interface HubInfoValue {
+    ready: boolean;
+    message: string;
+    nodes?: (NodesEntity)[] | null;
+}
+export interface NodesEntity {
+    id: string;
+    uri: string;
+    maxSessions: number;
+    osInfo: OsInfo;
+    heartbeatPeriod: number;
+    availability: string;
+    version: string;
+    slots?: (SlotsEntity)[] | null;
+}
+export interface OsInfo {
+    arch: string;
+    name: string;
+    version: string;
+}
+export interface SlotsEntity {
+    id: Id;
+    lastStarted: string;
+    session?: SessionEntity;
+    stereotype: Stereotype;
+}
+export interface Id {
+    hostId: string;
+    id: string;
+}
+export interface Stereotype {
+    browserName: string;
+    browserVersion: string;
+    platformName: string;
+    "se:noVncPort": number;
+    "se:vncEnabled": boolean;
+}
+
+export interface SessionEntity {
+    capabilities: Capabilities;
+    sessionId:    string;
+    start:        Date;
+    stereotype:   Stereotype;
+    uri:          string;
+}
+
+export interface Capabilities {
+    browserName:     string;
+    browserVersion:  string;
+    platformName:    string;
+}
+
+
+export type APIResult = {
+    "hub_status_response": {
+        Ok?: {
+            response: string
+        },
+        Err?: {
+            error: string
         }
-        return e;
+    },
+    "router_hub_state": {
+        ip: string,
+        port: number,
+        name: string
+    }
+};
+
+export type ApiStatusData = {
+    "hub_status_response": HubInfo | null,
+    err?: string
+    "router_hub_state": {
+        ip: string,
+        port: number,
+        name: string
+    }
+}
+
+function unmarshall(result: APIResult): ApiStatusData {
+    let status_response: HubInfo | null = JSON.parse(result.hub_status_response.Ok?.response ?? "null");
+    
+    return {
+        hub_status_response: status_response,
+        err: status_response === null ? result.hub_status_response.Err?.error : undefined,
+        router_hub_state: result.router_hub_state
+    }
+}
+
+function update_data(){
+    fetch(`${window.location.origin}/api/hubs/status`, {
+        method: "GET"
     })
-    .then(e => e.json())
-    .then(json => {
-        console.log("Got json: ", json);
-        api_data.set(json);
-    })
-    .catch(e => {
-        let error: ApiFetchError = {
-            is_error: true,
-            message: e.toString()
-        }
-        api_data.set(error);
-    });
+        .then(e => {
+            if (!e.ok) {
+                throw new Error(`Got invalid response fetching ${e.url}:  ${e.status} ${e.statusText}`)
+            }
+            return e;
+        })
+        .then(e => e.json())
+        .then(json => {
+            console.log("Unmarshalled:", json.map(unmarshall));
+    
+            api_data.set({
+                state: "Success",
+                data: json.map(unmarshall)
+            })
+        })
+        .catch(e => {
+            let error: ApiFetchError = {
+                state: "Error",
+                message: e.toString()
+            }
+            api_data.set(error);
+        });
+}
+
+update_data();
+setInterval(update_data, 1000);
+
+
+export function follow_breadcrumb(
+    breadcrumb: number[],
+    depth: number,
+    hubs: HubInfo[]
+) {
+    let hub = hubs[breadcrumb[0]];
+    if (depth === 0) return hub;
+    let node = hub.value.nodes[breadcrumb[1]];
+    if (depth === 1) return node;
+    let session = node.slots[breadcrumb[2]];
+    return session;
+}
+
+export function get_breadcrumb_siblings(
+    breadcrumb: number[],
+    depth: number,
+    hubs: HubInfo[]
+) {
+
+    if (depth === 0) return hubs;
+    let nodes = hubs[breadcrumb[0]].value.nodes;
+    if (depth === 1) return nodes;
+    let sessions = nodes[breadcrumb[1]].slots;
+    return sessions;
+}
